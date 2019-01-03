0533adf6f22eeca545ece1e19ec108b342093586
schwarzg <schwarzg@gist.ac.kr>
2019-01-03 21:49:46 +0900

    Upload test

---
+++ RNN.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+import tensorflow as tf
+
+def toy_problem(T=100.,amp=0.05):
+	x=np.arange(0,2*T+1)
+	noise=amp*np.random.uniform(low=-1,high=1,size=len(x))
+	return np.sin(2.0*np.pi*x/T)+noise
+
+
