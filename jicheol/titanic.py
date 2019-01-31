9af03d09a7ef1d5520c509a874661c343b06dff8
Jicheol Kim <jeechul040@naver.com>
2019-01-31 17:23:37 +0900

    titanic_v3

--- jicheol/titanic.py
+++ jicheol/titanic.py
@@ -1,10 +1,11 @@
 import keras
 import numpy as np
 import pandas as pd
+import csv
 import matplotlib.pyplot as plt 
 from keras.models import Sequential
 from keras.layers import Dense, Activation
-from keras.optimizers import SGD
+from keras.optimizers import SGD, Adam
 from keras.losses import binary_crossentropy
 
 TrainData = np.array(pd.read_csv('./titanic/train.csv'))
@@ -29,19 +30,19 @@ X_val[:,2] = np.array(map(lambda x : 0. if(np.isnan(x)) else x, list(ValData[:,4
 X_val[:,3:5] = ValData[:,5:7]
 X_val[:,5] = np.array(map(lambda x : 0. if(np.isnan(x)) else x, list(ValData[:,8])))
 
-norm = np.array([3.,1.,50.,8.,6.,512.])
+norm = np.array([3.,1.,80.,8.,6.,512.])
 X_train = X_train/norm
 X_val = X_val/norm
 
-n_hidden = 500
+n_hidden = 1000
 
 model = Sequential()
 model.add(Dense(n_hidden,activation='relu'))
 model.add(Dense(1,activation='sigmoid'))
 
-model.compile(loss=binary_crossentropy,optimizer=SGD(lr=0.01),metrics=['accuracy'])
+model.compile(loss=binary_crossentropy,optimizer=Adam(lr=0.001,beta_1=0.9,beta_2=0.999),metrics=['accuracy'])
 
-history = model.fit(X_train,Y_train,epochs=100,batch_size=20,verbose=1)
+history = model.fit(X_train,Y_train,epochs=500,batch_size=10,verbose=0,shuffle=True)
 
 plt.plot(history.history['acc'])
 plt.title('model accuracy')
@@ -57,6 +58,13 @@ plt.show()
 
 loss_and_metrics = model.evaluate(X_val,Y_test)
 print(loss_and_metrics)
+
+pred = model.predict_classes(X_val,batch_size=1)
+f = open('prediction.csv','w')
+wr = csv.writer(f)
+wr.writerow(['PassengerId','Survived'])
+for i in range(418):
+    wr.writerow([ValData[i,0],pred[i,0]])
 
 
 
